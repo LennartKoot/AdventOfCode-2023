5fa4d0fff3bd3710b60908fe35d474e5da9d5097
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-03 23:59:44 +0100

    chore: Prepare Day 02

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode;
+
+public class Day_02 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"Part 1");
+
+    public override ValueTask<string> Solve_2() => new($"Part 2");
+}
