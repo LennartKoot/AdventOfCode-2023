9c2fc4d363c01eb7c98535bdbc8cb6c1a5d4b7bd
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-03 23:08:41 +0100

    feat: Solve Day 01 Part 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,4 +1,4 @@
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public class Day_01 : BaseDay
 {
@@ -9,7 +9,38 @@ public class Day_01 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+
+    public int Solution_1() {
+        const char zero = '0';
+
+        var result = 0;
+        foreach (var line in _input) {
+            result += 10 * FirstDigit(line) + LastDigit(line);
+        }
+
+        int FirstDigit(string line) {
+            for (var i = 0; i < line.Length; i++) {
+                if (char.IsDigit(line[i])) {
+                    return line[i] - zero;
+                }
+            }
+
+            throw new SolvingException();
+        }
+
+        int LastDigit(string line) {
+            for (var i = line.Length - 1; i >= 0; i--) {
+                if (char.IsDigit(line[i])) {
+                    return line[i] - zero;
+                }
+            }
+
+            throw new SolvingException();
+        }
+
+        return result;
+    }
 }
