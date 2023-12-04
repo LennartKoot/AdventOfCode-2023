50ac7a2b5ccb9664b96a63d390c1a82035bc4fd2
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-04 21:06:38 +0100

    feat: Solve Day 04 Part 2

--- AdventOfCode/Day_04.cs
+++ AdventOfCode/Day_04.cs
@@ -11,7 +11,7 @@ public class Day_04 : BaseDay
 
     public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
-    public override ValueTask<string> Solve_2() => new($"Part 2");
+    public override ValueTask<string> Solve_2() => new($"{Solution_2()}");
 
     public int Solution_1() {
         var result = 0;
@@ -26,5 +26,32 @@ public class Day_04 : BaseDay
         }
 
         return result;
+    }
+
+    public int Solution_2() {
+        Dictionary<int, int> cards = [];
+        for (int i = 0; i < _input.Length; i++) {
+            if (!cards.TryAdd(i, 1))
+                cards[i]++;
+
+            var card = _input[i];
+
+            var splitted = card.Split([':', '|']);
+            var winning = splitted[1].Split(' ').Where(s => !string.IsNullOrEmpty(s));
+            var owned = splitted[2].Split(' ').Where(s => !string.IsNullOrEmpty(s));
+
+            var winningNumbers = owned.Count(winning.Contains);
+
+            var currentCardsOwned = cards[i];
+
+            for (int j = 1; j <= winningNumbers; j++) {
+                if (cards.TryGetValue(i + j, out int value))
+                    cards[i + j] = value + currentCardsOwned;
+                else
+                    cards.Add(i + j, currentCardsOwned);
+            }
+        }
+
+        return cards.Sum(kvp => kvp.Value);
     }
 }
