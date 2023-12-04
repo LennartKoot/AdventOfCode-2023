ad66a6fc53161d6f3cd450d34186b7210321ddeb
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-04 20:50:11 +0100

    feat: Solve Day 04 Part 1

--- AdventOfCode/Day_04.cs
+++ AdventOfCode/Day_04.cs
@@ -9,7 +9,22 @@ public class Day_04 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Part 1");
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Part 2");
+
+    public int Solution_1() {
+        var result = 0;
+        foreach (var card in _input) {
+            var splitted = card.Split([':', '|']);
+            var winning = splitted[1].Split(' ').Where(s => !string.IsNullOrEmpty(s));
+            var owned = splitted[2].Split(' ').Where(s => !string.IsNullOrEmpty(s));
+
+            var winningNumbers = owned.Count(winning.Contains);
+            if (winningNumbers > 0)
+                result += 1 << (winningNumbers - 1);
+        }
+
+        return result;
+    }
 }
