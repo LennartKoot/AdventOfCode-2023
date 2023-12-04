4753aecae652b047686c7350823bc098e5b474e2
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-04 17:59:55 +0100

    feat: Parse input Day 02

--- AdventOfCode/Day_02.cs
+++ AdventOfCode/Day_02.cs
@@ -1,12 +1,63 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
 
-public class Day_02 : BaseDay
+namespace AdventOfCode;
+
+public partial class Day_02 : BaseDay
 {
-    private readonly string[] _input;
+    public record CubesSubset(int Red, int Green, int Blue);
+    public record Game(int Id, List<CubesSubset> CubesSubsets);
+
+    private readonly List<Game> _input;
+
+    [GeneratedRegex(@"\d+(?=\sred)")]
+    private static partial Regex RedRegex();
+
+    [GeneratedRegex(@"\d+(?=\sgreen)")]
+    private static partial Regex GreenRegex();
+
+    [GeneratedRegex(@"\d+(?=\sblue)")]
+    private static partial Regex BlueRegex();
 
     public Day_02()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        _input = ParseInput();
+    }
+
+    private List<Game> ParseInput() {
+
+        var result = new List<Game>();
+        foreach(var line in File.ReadAllLines(InputFilePath)) {
+            var segments = line.Split([';', ':']);
+            var gameNumber = int.Parse(segments[0].Where(char.IsDigit).ToArray());
+
+            var subsets = new List<CubesSubset>();
+            for (int i = 1; i < segments.Length; i++) {
+                var colors = segments[i].Split(',');
+                var red = 0; var green = 0; var blue = 0;
+                foreach(var color in colors) {
+                    var redMatch = RedRegex().Match(color);
+                    if (redMatch.Success) {
+                       red = int.Parse(redMatch.Value);
+                       continue;
+                    }
+                    var greenMatch = GreenRegex().Match(color);
+                    if (greenMatch.Success) {
+                        green = int.Parse(greenMatch.Value);
+                        continue;
+                    }
+                    var blueMatch = BlueRegex().Match(color);
+                    if (blueMatch.Success) {
+                        blue = int.Parse(blueMatch.Value);
+                        continue;
+                    }
+                }
+                subsets.Add(new(red, green, blue));
+            }
+
+            result.Add(new Game(gameNumber, subsets));
+        }
+
+        return result;
     }
 
     public override ValueTask<string> Solve_1() => new($"Part 1");
