5d3776167d15b905f8c0c53f8294f59e4c99d1cf
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-05 21:58:58 +0100

    feat: Optimize solution Day 05 Part 2

--- AdventOfCode/Day_05.cs
+++ AdventOfCode/Day_05.cs
@@ -1,6 +1,4 @@
-﻿using Spectre.Console;
-
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public class Day_05 : BaseDay
 {
@@ -18,13 +16,13 @@ public class Day_05 : BaseDay
     }
 
     public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
-    public override ValueTask<string> Solve_2() => new($"{Solution_2()}");
+    public override ValueTask<string> Solve_2() => new($"{Solution_2_Optimized()}");
 
     public long Solution_1() {
         return _seeds.Select(GetLocation).Min();
     }
 
-    public long Solution_2() {
+    public long Solution_2_Original() {
         var seedRanges = new List<SeedRange>();
         for (int i = 0; i < _seeds.Length; i += 2)
             seedRanges.Add(new(_seeds[i], _seeds[i + 1]));
@@ -40,8 +38,70 @@ public class Day_05 : BaseDay
         }
     }
 
-    private long GetLocation(long seed) {
-        var source = seed;
+    public long Solution_2_Optimized() {
+        for (int i = 0; i < _maps.Length; i++) {
+            _maps[i].Sort((a, b) => a.SourceStart.CompareTo(b.SourceStart));
+        }
+        var seedRanges = new List<SeedRange>();
+        for (int i = 0; i < _seeds.Length; i += 2)
+            seedRanges.Add(new(_seeds[i], _seeds[i + 1]));
+
+        var minLocation = long.MaxValue;
+
+        foreach (var seedRange in seedRanges) {
+            var seedRangeMin = seedRange.Start;
+            var seedRangeMax = seedRangeMin + seedRange.Length;
+            var ranges = OverlapRanges(new(seedRangeMin, seedRangeMin, seedRangeMax - seedRangeMin), _maps[0]);
+            for (int i = 1; i < _maps.Length; i++) {
+                var newRanges = new List<Range>();
+                foreach (var range in ranges) {
+                    newRanges.AddRange(OverlapRanges(range, _maps[i]));
+                }
+                ranges = newRanges;
+            }
+            var localMinLocation = ranges.MinBy(r => r.SourceStart).SourceStart;
+            if (localMinLocation < minLocation)
+                minLocation = localMinLocation;
+        }
+
+        return minLocation;
+    }
+
+    private List<Range> OverlapRanges(Range range, List<Range> sortedSubRanges) {
+        if (sortedSubRanges.Count == 0) {
+            return [range];
+        }
+
+        var rangeMin = range.SourceStart; // Inclusive
+        var rangeMax = rangeMin + range.Length; //Exclusive
+
+        var newRanges = new List<Range>();
+
+        var endLastRange = rangeMin;
+        for (int i = 0; i < sortedSubRanges.Count; i++) {
+            var subRangeMax = sortedSubRanges[i].SourceStart + sortedSubRanges[i].Length;
+            if (subRangeMax <= rangeMin || sortedSubRanges[i].SourceStart >= rangeMax)
+                continue;
+
+            if (endLastRange < sortedSubRanges[i].SourceStart) {
+                newRanges.Add(new(endLastRange, endLastRange, sortedSubRanges[0].SourceStart - endLastRange));
+                endLastRange = sortedSubRanges[i].SourceStart;
+            }
+
+            var endOfRange = Math.Min(subRangeMax, rangeMax);
+            var indexInSubRange = endLastRange - sortedSubRanges[i].SourceStart;
+            var startDestination = sortedSubRanges[i].DestinationStart + indexInSubRange;
+            newRanges.Add(new(startDestination, startDestination, endOfRange - endLastRange));
+            endLastRange = endOfRange;
+        }
+
+        if (endLastRange < rangeMax)
+            newRanges.Add(new(endLastRange, endLastRange, rangeMax - endLastRange));
+
+        return newRanges;
+    }
+
+    private long GetLocation(long source) {
         for (int i = 0; i < 7; i++) {
             var range = _maps[i].Find(r => r.SourceStart <= source && r.SourceStart + r.Length > source);
             if (range != null) {
