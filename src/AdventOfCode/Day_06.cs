384e81e22fb4c5ba963f76fcdd553173256cdf2f
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-06 12:01:26 +0100

    feat: Optimize Day 06 Part 2 by using quadratic formula

--- src/AdventOfCode/Day_06.cs
+++ src/AdventOfCode/Day_06.cs
@@ -11,7 +11,7 @@ public class Day_06 : BaseDay
 
     public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
-    public override ValueTask<string> Solve_2() => new($"{Solution_2()}");
+    public override ValueTask<string> Solve_2() => new($"{Solution_2_QuadraticFormula()}");
 
     record Game(long Time, long Record);
 
@@ -37,11 +37,34 @@ public class Day_06 : BaseDay
             .Aggregate((a, b) => a * b);
     }
 
-    public long Solution_2() {
+    public long Solution_2_Original() {
         var time = ConvertDigitsToNumber(_input[0].Split(':')[1]);
         var record = ConvertDigitsToNumber(_input[1].Split(':')[1]);
 
         return CountDistancesPastRecord(new(time, record));
+    }
+
+    public long Solution_2_QuadraticFormula() {
+        var time = ConvertDigitsToNumber(_input[0].Split(':')[1]);
+        var record = ConvertDigitsToNumber(_input[1].Split(':')[1]);
+
+        /* => x * (T - x) = R
+         * => -(x^2) + T*x = R
+         * => x^2 - T*x = -R
+         * => x^2 - T*x + R = 0
+         * => a = 1, b = -T, c = R
+         * where T = time and R = record
+         */
+        (var x1, var x2) = QuadraticFormula(1, -time, record);
+        return (long)Math.Ceiling(x1) - (long)Math.Floor(x2) - 1; // -1 because x1 and x2 are exclusive bounds
+    }
+
+    private static (double, double) QuadraticFormula(long a, long b, long c) {
+        var sqrt = Math.Sqrt(b * b - 4 * a * c);
+        var x1 = (-b + sqrt) / (2 * a);
+        var x2 = (-b - sqrt) / (2 * a);
+
+        return (x1, x2);
     }
 
     private long CountDistancesPastRecord(Game game) {
