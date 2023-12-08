94c17faedf0069251f1a7f9474d7a9412c472d20
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-08 11:27:32 +0100

    feat: Solve Day 08 Part 1

--- src/AdventOfCode/Day_08.cs
+++ src/AdventOfCode/Day_08.cs
@@ -2,14 +2,41 @@
 
 public class Day_08 : BaseDay
 {
-    private readonly string[] _input;
+    private readonly char[] _instructions;
+    private readonly Dictionary<string,Node> _nodes = [];
 
     public Day_08()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        var input = File.ReadAllLines(InputFilePath);
+        _instructions = input[0].ToCharArray();
+
+        for (int i = 2; i < input.Length; i++) {
+            var line = input[i];
+            var label = line.Substring(0, 3);
+            var left = line.Substring(7, 3);
+            var right = line.Substring(12, 3);
+            _nodes.Add(label, new(label, left, right));
+        }
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution 1");
+    record Node(string Label, string Left, string Right);
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution 2");
+
+    public int Solution_1() {
+        var step = 0;
+
+        var node = _nodes["AAA"];
+        while(node.Label != "ZZZ") {
+            var instruction = _instructions[step % _instructions.Length];
+            if (instruction == 'L')
+                node = _nodes[node.Left];
+            else
+                node = _nodes[node.Right];
+            ++step;
+        }
+
+        return step;
+    }
 }
