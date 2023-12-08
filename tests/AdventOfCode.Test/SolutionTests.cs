592c00e2532071f975c6d7c2dae026b41f0e282b
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-08 21:36:48 +0100

    Day 08 (#3)
    * chore: Prepare Day 08
    
    * feat: Solve Day 08 Part 1
    
    * chore: Use real input for Day 08
    
    * feat: 'Solve' Day 08 Part 2
    
    * feat: Implement LCM to actually solve Part 2

--- tests/AdventOfCode.Test/SolutionTests.cs
+++ tests/AdventOfCode.Test/SolutionTests.cs
@@ -11,6 +11,7 @@ public static class SolutionTests
     [TestCase(typeof(Day_05), "486613012", "56931769")]
     [TestCase(typeof(Day_06), "781200", "49240091")]
     [TestCase(typeof(Day_07), "250453939", "248652697")]
+    [TestCase(typeof(Day_08), "14429", "10921547990923")]
     public static async Task Test(Type type, string sol1, string sol2)
     {
         if (Activator.CreateInstance(type) is BaseProblem instance)
