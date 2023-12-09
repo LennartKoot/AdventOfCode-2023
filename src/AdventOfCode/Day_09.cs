a95d539b50c589670fdf16db7692e9d46b1e1664
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-09 16:55:05 +0100

    feat: Solve Day 09 Part 1

--- src/AdventOfCode/Day_09.cs
+++ src/AdventOfCode/Day_09.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using Spectre.Console;
+
+namespace AdventOfCode;
 
 public class Day_09 : BaseDay
 {
@@ -9,7 +11,39 @@ public class Day_09 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution 1");
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution 2");
+
+    public long Solution_1() {
+        long result = 0;
+        foreach (var report in _input) {
+            var numbers = report.Split(' ').Select(n => (int?)int.Parse(n)).ToArray();
+            List<int?[]> sequences = [numbers];
+
+            var currentSequence = 0;
+            while (sequences[currentSequence][^1] != 0) {
+                sequences.Add(new int?[sequences[currentSequence].Length - 1]);
+                currentSequence++;
+                DetermineNumberInSequence(sequences, currentSequence);
+            }
+
+            for(; currentSequence >= 0; currentSequence--)
+                result += (int)sequences[currentSequence][^1];
+        }
+
+        return result;
+    }
+
+    private static int DetermineNumberInSequence(List<int?[]> sequences, int sequence, int indexFromEnd = 1) {
+        var lastValue = sequences[sequence][^indexFromEnd];
+        if (lastValue != null)
+            return (int)lastValue;
+
+        var left = DetermineNumberInSequence(sequences, sequence - 1, indexFromEnd + 1);
+        var right = DetermineNumberInSequence(sequences, sequence -1, indexFromEnd);
+        var value = right - left;
+        sequences[sequence][^indexFromEnd] = value;
+        return value;
+    }
 }
