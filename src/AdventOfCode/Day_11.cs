a606f3902a7fe2be9476d4e381417fd251c9d503
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-11 17:04:44 +0100

    feat: Solve Day 11 Part 1

--- src/AdventOfCode/Day_11.cs
+++ src/AdventOfCode/Day_11.cs
@@ -2,14 +2,48 @@
 
 public class Day_11 : BaseDay
 {
-    private readonly string[] _input;
-
-    public Day_11()
-    {
-        _input = File.ReadAllLines(InputFilePath);
-    }
-
-    public override ValueTask<string> Solve_1() => new($"Solution 1");
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution 2");
+
+    public int Solution_1() {
+        var galaxies = new List<(int, int)>();
+        var lines = File.ReadLines(InputFilePath).ToArray();
+        var y = 0;
+        foreach (var line in lines) {
+            var x = 0;
+            foreach (var @char in line) {
+                if (@char == '#')
+                    galaxies.Add((x, y));
+                ++x;
+            }
+            ++y;
+        }
+
+        var max_x = lines[0].Length;
+        var max_y = lines.Length;
+        var emptyRows = new List<int>();
+        var emptyColumns = new List<int>();
+        for (int i = 0; i < max_x; i++)
+            if (!galaxies.Any(g => g.Item1 == i))
+                emptyColumns.Add(i);
+        for (int i = 0; i < max_y; i++)
+            if (!galaxies.Any(g => g.Item2 == i))
+                emptyRows.Add(i);
+
+        var result = 0;
+        for (int i = 0; i < galaxies.Count; i++)
+            for (int j = i + 1; j < galaxies.Count; j++) {
+                var (galaxyX, galaxyY) = galaxies[i];
+                var (pairX, pairY) = galaxies[j];
+                galaxyX += emptyColumns.Count(c => c < galaxyX);
+                galaxyY += emptyRows.Count(r => r < galaxyY);
+                pairX += emptyColumns.Count(c => c < pairX);
+                pairY += emptyRows.Count(r => r < pairY);
+
+                result += Math.Abs(galaxyX - pairX) + Math.Abs(galaxyY - pairY);
+            }
+
+        return result;
+    }
 }
