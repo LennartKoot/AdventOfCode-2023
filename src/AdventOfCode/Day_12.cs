42ee88f64c0fadccee875267feaaebb74b4970f1
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-12 21:39:21 +0100

    feat: Solve Day 12 Part 1

--- src/AdventOfCode/Day_12.cs
+++ src/AdventOfCode/Day_12.cs
@@ -1,4 +1,8 @@
-﻿namespace AdventOfCode;
+﻿using Stack = System.Collections.Immutable.ImmutableStack<int>;
+using Cache = System.Collections.Generic.Dictionary<(string, System.Collections.Immutable.ImmutableStack<int>), long>;
+using System.Collections.Immutable;
+
+namespace AdventOfCode;
 
 public class Day_12 : BaseDay
 {
@@ -9,7 +13,60 @@ public class Day_12 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution 1");
+    public override ValueTask<string> Solve_1() => new($"{Solution_1()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution 2");
+
+    public long Solution_1() {
+        long result = 0;
+        foreach (var line in _input) {
+            var parts = line.Split(' ');
+            var springs = parts[0];
+            var numbers = parts[1].Split(',').Select(int.Parse);
+            result += GetPossibleArrangements(springs, ImmutableStack.CreateRange(numbers.Reverse()), new Cache());
+        }
+        return result;
+    }
+
+    private static long GetPossibleArrangements(string springs, Stack configuration, Cache cache) {
+        if (!cache.TryGetValue((springs, configuration), out var result)) {
+            var calculatedResult = CalculatePossibleArrangements(springs, configuration, cache);
+            cache[(springs, configuration)] = calculatedResult;
+            return calculatedResult;
+        }
+
+        return result;
+    }
+
+    private static long CalculatePossibleArrangements(string springs, Stack numbers, Cache cache) {
+        return springs.FirstOrDefault() switch {
+            '.' => GetPossibleArrangements(springs[1..], numbers, cache), // Continue recurse with operational spring removed.
+            '#' => HandleBrokenSpring(springs, numbers, cache),
+            '?' => GetPossibleArrangements("." + springs[1..], numbers, cache) + GetPossibleArrangements("#" + springs[1..], numbers, cache), // Recurse with trying both an operational and broken spring.
+            _   => !numbers.IsEmpty ? 0 : 1, // If numbers left at end of sequence, this is not a valid pattern.
+        };
+    }
+
+    private static long HandleBrokenSpring(string springs, Stack numbers, Cache cache) {
+        if (numbers.IsEmpty)
+            return 0; // Broken spring while configuration says no broken springs left.
+
+        numbers = numbers.Pop(out var n);
+
+        var potentialBrokenSprings = 0;
+        for (;potentialBrokenSprings < springs.Length; potentialBrokenSprings++) {
+            var spring = springs[potentialBrokenSprings];
+            if (spring == '.')
+                break;
+        }
+
+        if (potentialBrokenSprings < n)
+            return 0; // Can't reach the required amount of broken strings set in the configuration.
+        if (n == springs.Length)
+            return numbers.IsEmpty ? 1 : 0; // End of springs, if we still need broken strings this is an invalid arrangement.
+        if (springs[n] == '#')
+            return 0; // Current required sequence is directly followed by another broken spring, invalid.
+
+        return GetPossibleArrangements(springs[(n+1)..], numbers, cache); // Continue recurse with next sequence of springs.
+    }
 }
