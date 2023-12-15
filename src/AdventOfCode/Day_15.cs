81e87164fdedda91c22289b883a92d93e0e2ec1c
Lennart van Koot <lennartvkoot@gmail.com>
2023-12-15 09:25:20 +0100

    feat: Solve Day 15 Part 1

--- src/AdventOfCode/Day_15.cs
+++ src/AdventOfCode/Day_15.cs
@@ -2,14 +2,27 @@
 
 public class Day_15 : BaseDay
 {
-    private readonly string[] _input;
+    private readonly string _input;
 
     public Day_15()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath)[0];
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution 1");
+    public override ValueTask<string> Solve_1() => new($"{Solve()}");
 
     public override ValueTask<string> Solve_2() => new($"Solution 2");
+
+    public int Solve() {
+        var sum = 0;
+        foreach (var sequence in _input.Split(',')) {
+            var currentValue = 0;
+            foreach (var @char in sequence) {
+                currentValue = (currentValue + @char) * 17 % 256;
+            }
+            sum += currentValue;
+        }
+
+        return sum;
+    }
 }
